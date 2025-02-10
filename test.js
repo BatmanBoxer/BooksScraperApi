efc65c2490d79f26778632059d833d88770978d3
Darwin Koirala <darwinkoirala123@gmail.com>
2025-02-10 20:39:23 +0545

    try bypassing anti scraping measures 4

--- test.js
+++ test.js
@@ -10,13 +10,15 @@ route.get('/', async (req, res) => {
     const response = await axios.get(baseSearchUrl + "harry", {
       headers: {
         'User-Agent': req.headers['user-agent'],
+        'Referer': 'https://annas-archive.org/',  // or the page you're scraping from
+        'Origin': 'https://annas-archive.org/',
       }
     });
 
     const data = response.data;
     res.json({ data: data });
   } catch (error) {
-    res.json({ status: error});
+    res.json({ status: error });
   }
 });
 export default route
