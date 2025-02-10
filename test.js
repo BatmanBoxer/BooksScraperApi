afab7865df945b5d09f7174fa7997da9803b64bb
Darwin Koirala <darwinkoirala123@gmail.com>
2025-02-10 20:53:13 +0545

    try bypassing anti scraping measures 7

--- test.js
+++ test.js
@@ -7,19 +7,12 @@ const route = express.Router();
 route.get('/', async (req, res) => {
   try {
     console.log('Incoming Request Headers:', req.headers);
-    const response = await axios.get(baseSearchUrl + "harry", {
-      headers: {
-        'Accept':'text/html',
-        'User-Agent': req.headers['user-agent'],
-        'Referer': 'https://annas-archive.org/',
-        'Origin': 'https://annas-archive.org/',
-      }
-    });
+    const response = await axios.get("https://catfact.ninja/fact");
 
     const data = response.data;
     res.json({ data: data });
   } catch (error) {
-    res.json({ status: error,"req":response });
+    res.json({ status: error,"req":response});
   }
 });
 export default route
