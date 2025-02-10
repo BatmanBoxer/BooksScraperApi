1cf537775d2a3dd9d75803f882ef402962cab4dc
Darwin Koirala <darwinkoirala123@gmail.com>
2025-02-10 19:35:06 +0545

    aadded try catch in test

--- test.js
+++ test.js
@@ -7,10 +7,14 @@ const route = express.Router();
 
 
 route.get('/', async (req, res) => {
+  try{
     const request = await axios.get(baseSearchUrl+"harry")
     const data = request.data
-
     res.json({ data:data })
+  }catch(error){
+    res.json({ status:error })
+  }
+
 });
 
 export default route
